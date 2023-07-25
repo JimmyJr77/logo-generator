d20582b4cfa99f9c960adfc8d927ae7c4672f44a
jimmyjr77 <jimmyjr.obrien@gmail.com>
2023-07-25 18:05:49 -0400

    successfully wrote an html file to call and display the shape from a pregenerated css. i need to dynamically generate a css file to finish with the required variables.

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const questions = [
 ];
 
 function generateLogo(answers) {
-    const { shape, shapeColor, text, textColor } = answers;
+    const {shape, shapeColor, text, textColor} = answers;
   
     console.log('Logo Design Requirements:');
     console.log('Shape:', shape);
@@ -41,25 +41,31 @@ function generateLogo(answers) {
     console.log('Text:', text);
     console.log('Text Color:', textColor);
   
-    // Get the <body> element
-    const bodyElement = document.getElementById('logo');
+    // Generate the HTML content dynamically
+    const htmlContent = `
+        <!DOCTYPE html>
+        <html>
+            <head>
+            <link rel="stylesheet" href="Assets/style.css">
+            <title>Dynamic Logo</title>
+            </head>
+            <body>
+                <div id="${shape}"</div>
+            </body>
+        </html>
+    `;
   
-    // Create a new <div> element
-    const logoDivElement = document.createElement('div');
-  
-    // Set the ID and style for the new <div>
-    logoDivElement.id = shape;
-    logoDivElement.style.backgroundColor = shapeColor;
-  
-    // Add some text content to the new <div>
-    logoDivElement.textContent = text;
-  
-    // Append the new <div> as a child to the <body> element
-    bodyElement.appendChild(logoDivElement);
+    // Write the generated HTML content to a file
+    fs.writeFile('logo.html', htmlContent, (err) => {
+      if (err) {
+        console.error('Error writing logo.html:', err);
+      } else {
+        console.log('Logo HTML file generated successfully!');
+      }
+    });
   }
   
   inquirer.prompt(questions).then((answers) => {
     generateLogo(answers);
   });
-
 
